9d4336b998eaa1db67d409697450a17b19f369e5
Moffein <22742925+Moffein@users.noreply.github.com>
2022-06-25 05:14:54 -0700

    Add project files.

---
+++ FixDamageTrailNullref/Class1.cs
@@ -0,0 +1,29 @@
+﻿using RoR2;
+using UnityEngine;
+using BepInEx;
+using System.Linq;
+using System;
+
+namespace R2API.Utils
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class ManualNetworkRegistrationAttribute : Attribute
+    {
+    }
+}
+
+namespace FixDamageTrailNullref
+{
+    [BepInPlugin("com.Moffein.FixDamageTrailNullref", "Fix DamageTrail Nullref", "1.0.0")]
+    public class FixDamageTrailNullref : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.DamageTrail.FixedUpdate += (orig, self) =>
+            {
+                self.pointsList = self.pointsList.Where(p => p.segmentTransform != null).ToList();
+                orig(self);
+            };
+        }
+    }
+}
